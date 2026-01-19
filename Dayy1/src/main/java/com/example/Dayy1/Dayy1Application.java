a48c48699d131131edeac8322bffa11ace7faacf
BhumikaAgrawal12 <bhumikaagrawal1208@gmail.com>
2026-01-19 14:35:56 +0530

    Fixed Dayy1

---
+++ Dayy1/src/main/java/com/example/Dayy1/Dayy1Application.java
@@ -0,0 +1,13 @@
+package com.example.Dayy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dayy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dayy1Application.class, args);
+	}
+
+}
---
+++ Dayy1/src/test/java/com/example/Dayy1/Dayy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dayy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dayy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
