67db6799720fc5f1101bce5f73ea62fe52c1ec6c
BhumikaAgrawal12 <bhumikaagrawal1208@gmail.com>
2026-01-19 15:38:13 +0530

    Day2 part2 added

--- Day2_React/src/main/java/com/example/day2_react/controller/Home.java
+++ Day2_React/src/main/java/com/example/day2_react/controller/Home.java
@@ -3,6 +3,9 @@ package com.example.day2_react.controller;
 import com.example.day2_react.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 @RestController
 public class Home {
 
@@ -11,4 +14,16 @@ public class Home {
         StudentModel student=new StudentModel("Bhumika","@Bhumika112",101);
         return student;
     }
+
+    @GetMapping("/userdata")
+    public ArrayList<StudentModel> getStudents(){
+        ArrayList<StudentModel> students=new ArrayList<>();
+        students.add(new StudentModel("Alia","@alia.com",102));
+        students.add(new StudentModel("Archi","@archi.com",102));
+        students.add(new StudentModel("Ishika","@ishika.com",102));
+        students.add(new StudentModel("Himanshi","@himanshi.com",102));
+        students.add(new StudentModel("Raghav","@raghav.com",102));
+        return students;
+    }
+
 }
